ff8d672fa163a103fa32685d523a7d11044488e8
LiWenJun1122 <741819476@qq.com>
2019-09-16 20:12:57 +0800

    更新

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <div>
-      <van-image round width="10rem" height="10rem" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
+    <van-nav-bar
+  title="标题"
+  right-text="按钮"
+  @click-right="onClickRight"
+/>
+    <div class="bor">
+      <van-image round width="5rem" height="5rem" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+   methods: {
+    onClickRight() {
+      this.$toast('登录成功');
+    }
+  }
+
+};
 </script>
 <style>
+.van-image--round img {
+  margin: 0 auto;
+}
+.bor{
+  border-bottom: 1px solid #f5f5f5;
+  padding:10px 0px;
+}
 </style>
