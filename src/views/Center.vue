dd7390d42bb510ca385fb51b34910a8b0f31b074
LiWenJun1122 <741819476@qq.com>
2019-09-18 19:56:44 +0800

    更新

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -1,10 +1,38 @@
 <template>
   <div>
-    <van-nav-bar
-  title="标题"
-  right-text="按钮"
-  @click-right="onClickRight"
-/>
+    <div>
+      <van-nav-bar
+        title="标题"
+        right-text="按钮"
+        @click-right="showPopup"
+      />
+     <van-dialog
+      v-model="show"
+      title="登录"
+      show-cancel-button
+      >
+        <van-cell-group>
+          <van-field
+            v-model="username"
+            clearable
+            label="用户名"
+            right-icon="question-o"
+            placeholder="请输入用户名"
+            @click-right-icon="$toast('question')"
+            :label-width="xiao"
+          />
+        
+          <van-field
+            v-model="password"
+            type="password"
+            label="密码"
+            placeholder="请输入密码"
+            :label-width="xiao"
+          />
+          <router-link to="/reg">注册</router-link>
+        </van-cell-group>
+      </van-dialog>
+    </div>
     <div class="bor">
       <van-image round width="5rem" height="5rem" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
     </div>
@@ -14,9 +42,18 @@
 </template>
 <script>
 export default {
-   methods: {
-    onClickRight() {
-      this.$toast('登录成功');
+  data() {
+    return {
+      show: false,
+      value: "",
+      username: "",
+      password: "",
+      xiao:"45px"
+    };
+  },
+  methods: {
+    showPopup() {
+      this.show = true;
     }
   }
 };
@@ -25,8 +62,8 @@ export default {
 .van-image--round img {
   margin: 0 auto;
 }
-.bor{
+.bor {
   border-bottom: 1px solid #f5f5f5;
-  padding:10px 0px;
+  padding: 10px 0px;
 }
 </style>
--- src/views/Reg.vue
+++ src/views/Reg.vue
@@ -1,26 +1,51 @@
  <template>
   <div>
-  <van-cell-group>
-  <van-field
-    v-model="username"
-    required
-    clearable
-    label="用户名"
-    right-icon="question-o"
-    placeholder="请输入用户名"
-    @click-right-icon="tishi"
-  />
-
-  <van-field
-    v-model="password"
-    type="password"
-    label="密码"
-    placeholder="请输入密码"
-    required
-    :error-message="errorMessage"
-    @blur="upwd"
-  />
-</van-cell-group>
+      <van-nav-bar
+        title="标题"
+        left-text="返回"
+        right-text="完成"
+        left-arrow
+        @click-left="onClickLeft"
+        @click-right="onClickRight"
+      />
+      <van-cell-group>
+      <van-field
+        v-model="username"
+        required
+        clearable
+        label="用户名"
+        right-icon="question-o"
+        placeholder="请输入用户名"
+        @click-right-icon="tishi"
+        :error-message=" unameError"
+        @blur="uname"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        label="密码"
+        placeholder="请输入密码"
+        required
+        :error-message="errorMessage"
+        @blur="upwd"
+        maxlength="25"
+      />
+      <van-field
+        v-model="phone"
+        label="手机号"
+        placeholder="请输入手机号"
+        :error-message="telErrorMessage"
+        @blur="tel"
+        maxlength="11"
+      />
+      <van-radio-group style="padding: 10px 16px;" class="clearfix" v-model="radio">
+        <div style="text-align:left;width: 90px;float:left;">
+          <span>性别</span>
+        </div>
+        <van-radio style="float:left;margin-right:5px;" name="1" checked-color="#07c160">男</van-radio>
+        <van-radio style="float:left;" name="2" checked-color="#07c160">女</van-radio>
+      </van-radio-group>
+    </van-cell-group>
   </div>
 </template>
 <script>
@@ -29,10 +54,22 @@ export default {
     return {
       username:"",
       password:"",
-      errorMessage:""
+      errorMessage:"",
+      telErrorMessage:"",
+      phone:"",
+      unameError:"",
+      radio:1
     };
   },
   methods:{
+    uname(){
+      if(this.username==""){
+        this. unameError="账号不能为空"
+      }else{
+        this. unameError="";
+      }
+
+    },
     upwd(){
       if(this.password==""){
         this.errorMessage="密码不能为空"
@@ -40,11 +77,33 @@ export default {
         this.errorMessage="";
       }
     },
+    tel(){
+      if(this.phone==""){
+        this.telErrorMessage="手机号不能为空"
+      }else{
+        this.telErrorMessage=""
+      }
+    },
     tishi(){
       this.$toast('*号部分为必填项,无则不填')
+    },
+    onClickLeft() {
+      this.$toast('返回');
+    },
+    onClickRight() {
+      this.$toast('按钮');
     }
   }
 };
 </script>
 <style>
+  .clearfix {
+}
+.clearfix::after {
+  content: ".";
+  display: block;
+  height: 0;
+  visibility: hidden;
+  clear: both;
+}
 </style>
